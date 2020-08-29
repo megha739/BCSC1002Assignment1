482c2f9f9067c62c729c3f118d224d809fa1a19c
megha <megha.nigam_cs19@gla.ac.in>
2020-08-30 03:16:27 +0530

    create getter and setter method  for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,7 +1,16 @@
  /*  User: megha nigam
- *  File Name : Library.java
- * */
-package definitions;
-public class Library{
- private Book[] booksCurrentlyAvailable;
+  *  File Name : Library.java
+  * */
+ package definitions;
+
+ public class Library {
+     private Book[] booksCurrentlyAvailable;
+
+     public Book[] getBooksCurrentlyAvailable() {
+         return booksCurrentlyAvailable.clone();
+     }
+
+     public void setBooksCurrentlyAvailable(Book[] booksCurrentlyAvailable) {
+         this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+     }
  }
