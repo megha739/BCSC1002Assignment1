17b79cf37283785a462a9ca2bc94c6feb0a5003c
megha <megha.nigam_cs19@gla.ac.in>
2020-08-30 05:18:57 +0530

    create getter and setter methods for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -9,7 +9,7 @@ public class Student {
     private String lastNameOfStudent;
     private long studentUniversityRollNo;
     private int numberOfBooksIssued;
-    private Book[] BooksIssuedByStudent;
+    private Book[] booksIssuedByStudent;
 
     public Student(String firstNameOfStudent, String middleNameOfStudent, String lastNameOfStudent, long studentUniversityRollNo, int numberOfBooksIssued) {
         this.firstNameOfStudent = firstNameOfStudent;
@@ -20,6 +20,54 @@ public class Student {
     }
 
     public Student() {
-        this.BooksIssuedByStudent = new Book[20];
+        this.booksIssuedByStudent = new Book[20];
+    }
+
+    public String getFirstNameOfStudent() {
+        return firstNameOfStudent;
+    }
+
+    public String getMiddleNameOfStudent() {
+        return middleNameOfStudent;
+    }
+
+    public String getLastNameOfStudent() {
+        return lastNameOfStudent;
+    }
+
+    public long getStudentUniversityRollNo() {
+        return studentUniversityRollNo;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public Book[] getBooksIssuedByStudent() {
+        return booksIssuedByStudent.clone();
+    }
+
+    public void setFirstNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+    }
+
+    public void setMiddleNameOfStudent(String middleNameOfStudent) {
+        this.middleNameOfStudent = middleNameOfStudent;
+    }
+
+    public void setLastNameOfStudent(String lastNameOfStudent) {
+        this.lastNameOfStudent = lastNameOfStudent;
+    }
+
+    public void setStudentUniversityRollNo(long studentUniversityRollNo) {
+        this.studentUniversityRollNo = studentUniversityRollNo;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public void setBooksIssuedByStudent(Book[] booksIssuedByStudent) {
+        this.booksIssuedByStudent = booksIssuedByStudent;
     }
 }
