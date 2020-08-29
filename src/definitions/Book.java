251ffaab4da3a883e6588756b6d63e52a0cb55db
megha <megha.nigam_cs19@gla.ac.in>
2020-08-30 01:49:17 +0530

    create constructors for fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -5,7 +5,19 @@ package definitions;
 
 public class Book {
     private String nameOfBook;
-    private String nameOfAuthors;
+    private String nameOfAuthor;
     // ISBN number of book Should be of 13 Digit .
     private String isbnNumberOfBook;
+
+    public Book() {
+        this.nameOfBook = "Design Patterns";
+        this.nameOfAuthor = "Erich Gamma";
+        this.isbnNumberOfBook = "1234567890112";
+    }
+
+    public Book(String nameOfBook, String nameOfAuthor, String isbnNumberOfBook) {
+        this.nameOfBook = nameOfBook;
+        this.nameOfAuthor = nameOfAuthor;
+        this.isbnNumberOfBook = isbnNumberOfBook;
+    }
 }
