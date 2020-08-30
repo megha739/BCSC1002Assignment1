64ff3de134ac4d029cd8d456a5b3bc6afc337b42
megha <megha.nigam_cs19@gla.ac.in>
2020-08-30 06:59:19 +0530

    create structure of FrontDesk

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -3,6 +3,8 @@
  * */
 package execution;
 
+import java.util.Scanner;
+
 import definitions.Book;
 import definitions.Library;
 import definitions.Student;
@@ -14,5 +16,55 @@ public class FrontDesk {
     private static final int SHOW_ALL_BOOKS_ISSUED = 3;
 
     public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        Book book = new Book();
+        Library library = new Library();
+        Student student = new Student();
+        System.out.println(" enter your first name");
+        String firstName = scannerObject.nextLine();
+        System.out.println("ENTER YOUR MIDDLE NAME:");
+        String middleName = scannerObject.nextLine();
+        System.out.println("ENTER YOUR LAST NAME:");
+        String lastName = scannerObject.nextLine();
+        System.out.println("ENTER YOUR ROLL NUMBER:");
+        Long rollNumber = scannerObject.nextLong();
+        scannerObject.nextLine();
+        System.out.println("-=-=--=-=-\"Welcome," + firstName + ",To The Front Desk\"-=-=--=-=-");
+        System.out.println("How may I help you today?");
+        System.out.println("1. Issue a new book for me.");
+        System.out.println("2. Return a previously issues book for me.");
+        System.out.println("3. Show me all my issues books.");
+        System.out.println("4. Exit.");
+        System.out.println("ENTER YOUR CHOICE FROM: [1,2,3,4]");
+        int choice = scannerObject.nextInt();
+        String bookName;
+        int booksIssued;
+        if (choice <= 4) {
+            switch (choice) {
+                case ISSUE_A_BOOK:
+                    System.out.println("ENTER THE NAME OF BOOK, YOU WANT TO ISSUE:");
+                    bookName = scannerObject.nextLine();
+                    scannerObject.nextLine();
+                    library.doIssue(bookName);
+                    break;
+                case RETURN_PREVIOUSLY_ISSUED_BOOKS:
+                    System.out.println("ENTER THE NAME OF BOOK,YOU WANT TO RETURN:");
+                    scannerObject.nextLine();
+                    bookName = scannerObject.nextLine();
+                    library.doReturn(bookName);
+                    break;
+                case SHOW_ALL_BOOKS_ISSUED:
+                    System.out.println("ENTER THE NUMBER OF BOOKS ISSUED");
+                    booksIssued = scannerObject.nextInt();
+                    student.getBooksIssuedByStudent();
+                    System.out.println(booksIssued);
+                    break;
+                case EXIT:
+                    break;
+            }
+        } else {
+            System.out.println("PLEASE CHOOSE THE CORRECT OPTION???");
+        }
+        scannerObject.close();
     }
 }
