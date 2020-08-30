d8bf6c6c601c0b12d36d68e10b0572737209a28f
megha <megha.nigam_cs19@gla.ac.in>
2020-08-30 05:34:36 +0530

    create fields for FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,8 +1,18 @@
 /*  User: megha nigam
-*  File Name : FrontDesk.java
+ *  File Name : FrontDesk.java
  * */
 package execution;
+
+import definitions.Book;
+import definitions.Library;
+import definitions.Student;
+
 public class FrontDesk {
+    public static final int EXIT = 4;
+    private static final int ISSUE_A_BOOK = 1;
+    private static final int RETURN_PREVIOUSLY_ISSUED_BOOKS = 2;
+    private static final int SHOW_ALL_BOOKS_ISSUED = 3;
+
     public static void main(String[] args) {
     }
 }
